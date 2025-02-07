59fe3ec813581fd7062ae27ca55a4ad499a10229
amt283 <athompson283@mycr.redwoods.edu>
2025-02-07 22:06:04 +0000

    Updated to add reflection

--- ch1-ch2-exercise.py
+++ ch1-ch2-exercise.py
@@ -10,6 +10,8 @@
 # I double checked this with a virtual assistant which verified this by saying:
 # "Python uses a rounding method called "round half to even," which means that if a number ends in 0.5, it will round to the nearest even integer."
 
+# Reflection: When comparing this with the instructor's, it looks like we had the same results.
+
 
 # Exercise 1.9.3
 
@@ -23,6 +25,8 @@
 # "2++2" acts like 2+2 and makes the variable "4"
 # If you have two values with no operator, like "4 2", you get a SyntaxError
 # If you call a function like "round()" and you forget one or both parenthesis then you get a syntax error
+
+# Reflection: When comparing this with the instructor's, it looks like we had the same results.
 
 
 # Exercise 1.9.4
@@ -45,6 +49,8 @@ print(type(abs(-7.0)))
 print(type(abs))
 print(type(int))
 print(type(type))
+
+# Reflection: When comparing this with the instructor's, it looks like we had the same results.
 
 
 # Exercise 1.9.4
@@ -73,6 +79,8 @@ total_hours = total_minutes / 60
 avg_pace_hr = total_hours / miles
 print(f"5. Average pace would be {avg_pace_hr:.2f} hours per mile.\n")
 
+# Reflection: When comparing this with the instructor's, it looks like we had the similar results.
+
 
 # Chapter 2 - Exercise 2.11.2
 
@@ -89,6 +97,9 @@ print(f"5. Average pace would be {avg_pace_hr:.2f} hours per mile.\n")
 # Adding a period at the end of a statement causes the same effect as the semi-colon 
 # Finally, trying to import "maath" causes a "ModuleNotFoundError" error
 
+# Reflection: When comparing this with the instructor's, it looks like we had the same results.
+# Though I wasn't sure what to do with "x = y = 1" and I just figured it didn't run, so it was interesting
+# to see that this is a valid way of assigning a value to two different variables at the same time.
 
 # Exercise 2.11.3
 
@@ -104,6 +115,9 @@ volume = (4/3)*pi*(radius**3)
 print(f"Exercise 2.11.3")
 print(f"1. The volume of a sphere with a radius of {radius} is {volume:.1f} cubic centimeters\n")
 
+# Reflection: When comparing this with the instructor's, it looks like we had the same results.
+
+
 # Part 2
 x = 42
 cos_x = math.cos(x)
@@ -111,10 +125,16 @@ sin_x = math.sin(x)
 sum = (cos_x**2)+(sin_x**2)
 print(f"2. The result is {sum:.1f}.\n")
 
+# Reflection: When comparing this with the instructor's, it looks like the instructor's
+# results are more efficient and check to see if the pythagorean identity is true
+# So rather than having a "yes/no" answer hard coded in, it'll check for you
+# and then pick the appropriate answer. I like this much better than my own because if there is a value
+# that isn't true, the program is already designed to react to that and nothing would need to be changed.
+
+
 # Part 3
 print(f"3a. Exponentiation operator: {math.e**2}")
 print(f"3b. math.pow: {math.pow(math.e,2)}")
 print(f"3c. math.exp: {math.exp(2)}")
 
-
-
+# Reflection: When comparing this with the instructor's, it looks like we had the same results.
